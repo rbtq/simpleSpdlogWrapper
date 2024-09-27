4b758ba48e5b448e7be6139206e928db682f1b42
rbtq <54366016+rbtq@users.noreply.github.com>
2024-09-27 17:26:04 +0100

    file cleanup and tutorial addition

--- spdlogWrapper/source/main.cpp
+++ spdlogWrapper/source/main.cpp
@@ -20,14 +20,14 @@ int main() {
 
 	//do stuff
 	programLogger.trace("Test1");
-	programLogger.trace("Test {0}", 0);
+	programLogger.trace("Test {0} A", 0); //{0} is how you add non-string parameters into the message
 	programLogger.debug("Test {0}", 1);
 	programLogger.info("Test {0}", 2);
 	programLogger.warn("Test {0}", 3);
-	programLogger.error("Test {0}", 4);
-	programLogger.critical("Test {0} {1}", 5, 10);
+	programLogger.error("Test {0} {1}", 4, 10); //can use {1,2,3} etc if you need to add more values into the message
+	programLogger.critical("Test {1} {0}", 5, 10); //does not need to be in order
 
-	//run stop function after logging
+	//run stop function after logging, cleanup
 	programLogger.stop();
 
 	return 0;
